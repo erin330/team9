795d28018404646db43321edc24c633a41a8ac2c
Decker, Nick <nick.decker@otto.de>
2023-11-26 21:55:41 +0900

    added second junit test and refactored setup to BeforeAll method

--- test/engine/DrawManagerTest.java
+++ test/engine/DrawManagerTest.java
@@ -11,14 +11,18 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 class DrawManagerTest {
+    private static Screen mockScreen;
+    private static Frame mockFrame;
+    private static BufferedImage mockBackBuffer;
+    private static Graphics mockBackBufferGraphics;
+    private static DrawManager testDrawManager;
 
-    @org.junit.jupiter.api.Test
-    @DisplayName("Mock test for drawHighScore")
-    void drawHighScore() {
-        Screen mockScreen = mock(Screen.class);
-        Frame mockFrame = mock(Frame.class);
-        BufferedImage mockBackBuffer = mock(BufferedImage.class);
-        Graphics mockBackBufferGraphics = mock(Graphics.class);
+    @org.junit.jupiter.api.BeforeAll
+    static void setuoTest(){
+        mockScreen = mock(Screen.class);
+        mockFrame = mock(Frame.class);
+        mockBackBuffer = mock(BufferedImage.class);
+        mockBackBufferGraphics = mock(Graphics.class);
 
         when(mockScreen.getWidth()).thenReturn(100);
         when(mockScreen.getHeight()).thenReturn(100);
@@ -29,23 +33,33 @@ class DrawManagerTest {
 
         when(mockBackBufferGraphics.getFontMetrics()).thenReturn(mock(FontMetrics.class));
 
-        ArgumentCaptor<String> argument = ArgumentCaptor.forClass(String.class);
-
-        DrawManager testDrawManager = DrawManager.getInstance();
+        testDrawManager = DrawManager.getInstance();
         testDrawManager.setFrame(mockFrame);
         testDrawManager.initDrawing(mockScreen, mockBackBuffer);
 
-        verify(mockScreen, times(1)).getWidth();
-        verify(mockScreen, times(1)).getWidth();
+    }
 
+    @org.junit.jupiter.api.Test
+    @DisplayName("Example test for drawHighScore")
+    void drawHighScore() {
+        ArgumentCaptor<String> argument = ArgumentCaptor.forClass(String.class);
 
         testDrawManager.drawHighScore(mockScreen, 1);
-
-        verify(mockScreen, times(2)).getWidth();
-        verify(mockBackBufferGraphics, times(1)).drawString(anyString(), anyInt(), anyInt());
-        verify(mockBackBufferGraphics).drawString(argument.capture(), anyInt(), anyInt());
+        verify(mockBackBufferGraphics, atLeastOnce()).drawString(argument.capture(), anyInt(), anyInt());
         assertEquals("0001", argument.getValue());
+    }
 
+    @org.junit.jupiter.api.Test
+    @DisplayName("Example test for drawLives")
+    void drawAmmo() {
+        ArgumentCaptor<String> drawString = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<Integer> horizontal = ArgumentCaptor.forClass(Integer.class);
+        ArgumentCaptor<Integer> vertical = ArgumentCaptor.forClass(Integer.class);
 
+        testDrawManager.drawAmmo(mockScreen, 5, 0);
+        verify(mockBackBufferGraphics, atLeastOnce()).drawString(drawString.capture(), horizontal.capture(), vertical.capture());
+        assertEquals("BUL: 10/5", drawString.getValue());
+        assertEquals(10, horizontal.getValue());
+        assertEquals(125, vertical.getValue());
     }
 }
